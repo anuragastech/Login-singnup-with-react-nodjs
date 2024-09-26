66a9313180dd071d22d872bdc7a87dae60ac748d
Anurag <anuragraveendren98@gmail.com>
2024-09-26 23:12:03 +0530

    host

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Login-singnup-with-react-nodjs/',
-});
+  base: '/Login-singnup-with-react-nodjs/', // Ensure this matches your repository name
+  build: {
+    rollupOptions: {
+      // If you have any other configurations, they go here
+    }
+  }
+})
