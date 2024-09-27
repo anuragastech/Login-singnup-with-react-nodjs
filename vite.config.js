630fd2502af4f23b19c0cd1417553e4d56c40cbe
Anurag <anuragraveendren98@gmail.com>
2024-09-28 01:36:38 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/Login-singnup-with-react-nodjs/',
   build: {
     rollupOptions: {
-      // If you have any other configurations, they go here
+      external: ['/assets/index-5Aj7_fpE.js'], 
     }
   }
 })
