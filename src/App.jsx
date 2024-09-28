a27228ba3cce1e39ad4173cef4a179c0ab87c779
Anurag <anuragraveendren98@gmail.com>
2024-09-28 13:09:46 +0530

    done1

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,12 @@ const App = () => {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<SignUp />} />
-          <Route path='/' element={<Login />} />
-          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          {/* Optional: Catch-all route */}
+          <Route path="*" element={<Login />} /> 
         </Routes>
       </BrowserRouter>
     </div>
