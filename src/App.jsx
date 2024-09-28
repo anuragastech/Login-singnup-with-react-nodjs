ada150fd9f6262a42c1f98495eb660151cddd25e
Anurag <anuragraveendren98@gmail.com>
2024-09-28 15:23:06 +0530

    donefinal

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 import Login from './components/LoginSignup/login';
 import SignUp from './components/LoginSignup/signup';
@@ -8,16 +8,14 @@ import Dashboard from './components/LoginSignup/dashboard';
 const App = () => {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          {/* Optional: Catch-all route */}
-          <Route path="*" element={<Login />} /> 
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/' element={<Login />} />
+          <Route path='/dashboard' element={<Dashboard />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
